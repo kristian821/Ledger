82138a4fe7ed68cd0da165bfc9cc1419257b73c9
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2023-10-08 18:05:05 -0700

    Initial Commit

---
+++ Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Kristian Mitchell on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
